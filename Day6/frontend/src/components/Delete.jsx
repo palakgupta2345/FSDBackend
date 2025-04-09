5a8ddf81a0f49e3dc05ed2cf59db146f5d2df8c0
palakgupta2345 <palakgupta0421@gmail.com>
2025-04-09 12:00:08 +0530

    final

--- Day6/frontend/src/components/Delete.jsx
+++ Day6/frontend/src/components/Delete.jsx
@@ -4,7 +4,7 @@ const Delete = () => {
   const handleDelete = async (e) => {
     e.preventDefault()
     const id = e.target.id.value;
-    await axios.delete(`https://fsdbackend-zeub.onrender.com/users${id}`)
+    await axios.delete(`https://fsdbackend-zeub.onrender.com/users/${id}`)
     alert('Product deleted successfully')
   }
   return (
--- Day6/frontend/src/components/View.jsx
+++ Day6/frontend/src/components/View.jsx
@@ -7,7 +7,7 @@ const View = () => {
     handleview();
   }, []);
   const handleview = async () => {
-    const res = await axios.get("https://fsdbackend-zeub.onrender.com/users");
+    const res = await axios.get("https://fsdbackend-zeub.onrender.com/users/");
     setUsers(res.data);
     console.log(users);
   };
