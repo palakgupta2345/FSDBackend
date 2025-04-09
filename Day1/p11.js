41de0c5c5a7d9243c4c3460aafad3da852f6e064
palakgupta2345 <palakgupta0421@gmail.com>
2025-04-09 10:59:17 +0530

    Initial backend and frontend code push

--- Day1/p11.js
+++ Day1/p11.js
@@ -2,4 +2,4 @@ const {sum,diff}=require('./p10.js');
 
 console.log(sum(5,7));
 
-console.log(diff(5,8));
+console.log(diff(5,8)); 
---
+++ Day6/frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Register from './components/Register'
+import View from './components/View';
+
+const App = () => {
+  return (
+    <div>
+      <Register/>
+    </div>
+  )
+};
+
+export default App
